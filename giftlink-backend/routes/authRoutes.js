3ff5f3debc62147a3e4d0a24cb9641c03bde44f4
Aldin Dautović <aldin33dautovic2@gmail.com>
2025-06-28 13:57:35 -0400

    Module 4 Login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -17,11 +17,6 @@ const logger = pino();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
-        console.log('POST /api/auth/register hit with body:', req.body);
-        if (!req.body.email || !req.body.password || !req.body.firstName || !req.body.lastName) {
-            return res.status(400).json({ error: 'Missing required fields' });
-          }
-        
     try {
         // Task 1: Connect to giftsdb in MongoDB
         const db = await connectToDatabase();
@@ -59,6 +54,52 @@ router.post('/register', async (req, res) => {
 
         // Respond with the token and email
         res.json({ authtoken, email: req.body.email });
+
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        // Task 7: Send appropriate message if user not found
+        if (!theUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Task 4: Check if the password matches the encrypted password
+        const isMatch = await bcryptjs.compare(req.body.password, theUser.password);
+        if (!isMatch) {
+            logger.error('Passwords do not match');
+            return res.status(401).json({ error: 'Wrong password' });
+        }
+
+        // Task 5: Fetch user details
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+
+        // Task 6: Create JWT authentication with user._id as payload
+        const payload = {
+            user: {
+                id: theUser._id.toString()
+            }
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        logger.info('User logged in successfully');
+        res.json({ authtoken, userName, userEmail });
 
     } catch (e) {
         logger.error(e);
